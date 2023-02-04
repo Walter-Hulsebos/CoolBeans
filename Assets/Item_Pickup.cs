a6a205008669885391413526049450ae8926cf37
Jeuneau <boekhoudtjeuneau@gmail.com>
2023-02-04 23:55:08 +0100

    Item_pickup_I_tried

--- Assets/Item_Pickup.cs
+++ Assets/Item_Pickup.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,24 +15,55 @@ namespace CoolBeans
 		{
 			bean = FindFirstObjectByType<Duplicate>();
 			bean.transform.position = this.transform.position;
+			
 
 		}
 
 		public void OnTriggerEnter2D(Collider2D other)
 		{
 			var x = other.GetComponent<Duplicate>();
-
+			Debug.Log("oop");
 			if (x != null)
 			{
 
 				GameObject square = this.gameObject;
 				x.food++;
 				Destroy(square);
-				
-				
-				
 			}
 		}
 	}
 			
+}*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CoolBeans
+{
+	public class Item_Pickup : MonoBehaviour
+	{
+		public GameObject square;
+		public int food;
+		public Duplicate bean;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			bean = FindObjectOfType<Duplicate>();
+			bean.transform.position = this.transform.position;
+		}
+
+		public void OnTriggerEnter2D(Collider2D other)
+		{
+			var x = other.GetComponent<Duplicate>();
+			if (x != null)
+			{
+				x.food++;
+				Destroy(this.gameObject);
+			}
+		}
+	}
 }
+
+
